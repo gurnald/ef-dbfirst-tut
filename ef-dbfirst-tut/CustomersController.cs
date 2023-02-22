c6eb276993f867c1bfba1a088947a31a6eb7091b
Jacob Roberts <jacobroberts9797@hotmail.com>
2023-02-22 16:31:25 -0500

    Updates

---
+++ ef-dbfirst-tut/CustomersController.cs
@@ -0,0 +1,55 @@
+﻿using ef_dbfirst_tut.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ef_dbfirst_tut;
+
+public class CustomersController {
+
+    private readonly SalesDbContext _context;
+
+    public CustomersController() {
+        _context = new SalesDbContext();
+    }
+
+    public async Task<List<Customer>> GetAllAsync() {
+        return await _context.Customers.ToListAsync();
+
+    }
+
+    public async Task<Customer> GetByIdAsync(int id) {
+        return await _context.Customers.FindAsync(id);
+
+    }
+
+    public async Task<bool> InsertAsync(Customer cust) {
+        _context.Customers.Add(cust);
+       var changes = await _context.SaveChangesAsync();
+        return (changes == 1) ? true : false;
+    }
+
+
+    public async Task<bool> UpdateAsync(Customer cust) {
+        var customers = await _context.Customers.FindAsync(cust.Id);
+        if (customers is null) {
+            throw new Exception("Not Found!");
+        }
+        _context.Entry(cust).State = EntityState.Modified;
+        var changes = await _context.SaveChangesAsync();
+        return (changes == 1) ? true : false;
+    }
+
+    public async Task<bool> DeleteAsync(int id) {
+        var customer = await _context.Customers.FindAsync(id);
+        if (customer is null) {
+            throw new Exception("Not Found!");
+        }
+        _context.Customers.Remove(customer);
+        var changes = await _context.SaveChangesAsync();
+        return (changes == 1) ? true : false;
+     }
+}
--- ef-dbfirst-tut/Program.cs
+++ ef-dbfirst-tut/Program.cs
@@ -1,18 +1,82 @@
-﻿using ef_dbfirst_tut.Models;
+﻿using ef_dbfirst_tut;
+using ef_dbfirst_tut.Models;
+using Microsoft.EntityFrameworkCore;
 
-var dbc = new SalesDbContext();
 
-var ordersWithCustomers = from o in dbc.Orders
-                          join c in dbc.Customers
-                              on o.CustomerId equals c.Id
-                          orderby c.Name    
-                          select new {
-                                Id = o.Id, Desc = o.Description, Customer = c.Name 
-                              };
+var custCtrl = new CustomersController();
 
-foreach(var oc in ordersWithCustomers) {
-    Console.WriteLine($"{oc.Id,2} | {oc.Desc,-30} | {oc.Customer,-30}");
-}
+var success = await custCtrl.DeleteAsync(39);
+
+Console.WriteLine(success ? "OK" : "FAILED");
+
+
+
+
+//var success = await custCtrl.UpdateAsync(bootcamp);
+
+//var bootcamp = await custCtrl.GetByIdAsync(39);
+//
+//bootcamp.Sales = 5000;
+
+
+//var cust = new Customer() {
+//    Id = 0,
+//    Name = "Bootcamp",
+//    City = "Mason",
+//    State = "OH",
+//    Sales = 0,
+//    Active = true
+//};
+//
+//var success = await custCtrl.InsertAsync(cust);
+
+
+
+
+
+//var cust = await custCtrl.GetByIdAsync(1);
+//Console.WriteLine(cust.Name);
+
+
+
+//foreach(var cust in await custCtrl.GetAllAsync()) {
+//    Console.WriteLine(cust.Name);
+//
+//}
+
+
+
+//var dbc = new SalesDbContext();
+
+//var customer = await GetById(1);
+
+//var customers = await GetAll();
+
+//foreach(var cust in customers) {
+//    Console.WriteLine(cust.Name);
+//} 
+//Console.WriteLine(customer.Name);
+
+//async Task<Customer> GetById(int id) {
+//        return await dbc!.Customers.FindAsync(id);
+//}
+
+ //async Task<List<Customer>> GetAll() {
+ //  return await dbc.Customers.ToListAsync();
+//}
+
+
+//var ordersWithCustomers = from o in dbc.Orders
+//                          join c in dbc.Customers
+//                              on o.CustomerId equals c.Id
+//                          orderby c.Name    
+//                          select new {
+//                                Id = o.Id, Desc = o.Description, Customer = c.Name 
+//                              };
+//
+//foreach(var oc in ordersWithCustomers) {
+//    Console.WriteLine($"{oc.Id,2} | {oc.Desc,-30} | {oc.Customer,-30}");
+//}
 
 //var orders = from o in dbc.Orders
 //             select o;
