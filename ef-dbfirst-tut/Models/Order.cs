fd0749bc4b005720d8a508b93f472083a5295aae
Jacob Roberts <jacobroberts9797@hotmail.com>
2023-02-22 13:45:00 -0500

    Add project files.

---
+++ ef-dbfirst-tut/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ef_dbfirst_tut.Models;
+
+public partial class Order
+{
+    public int Id { get; set; }
+
+    public int? CustomerId { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual Customer? Customer { get; set; }
+
+    public virtual ICollection<OrderLine> OrderLines { get; } = new List<OrderLine>();
+}
---
+++ ef-dbfirst-tut/Program.cs
@@ -0,0 +1,31 @@
+﻿using ef_dbfirst_tut.Models;
+
+var dbc = new SalesDbContext();
+
+var ordersWithCustomers = from o in dbc.Orders
+                          join c in dbc.Customers
+                              on o.CustomerId equals c.Id
+                          orderby c.Name    
+                          select new {
+                                Id = o.Id, Desc = o.Description, Customer = c.Name 
+                              };
+
+foreach(var oc in ordersWithCustomers) {
+    Console.WriteLine($"{oc.Id,2} | {oc.Desc,-30} | {oc.Customer,-30}");
+}
+
+//var orders = from o in dbc.Orders
+//             select o;
+//
+//foreach(var order in orders) {
+//    Console.WriteLine(order.Description);
+//}
+//
+//var customers = dbc.Customers
+//    .Where(x => x.City == "Cincinnati")
+//    .OrderByDescending(x => x.Sales)
+//    .ToList();
+//
+//foreach (var customer in customers) {
+//    Console.WriteLine(customer.Name);
+//}
